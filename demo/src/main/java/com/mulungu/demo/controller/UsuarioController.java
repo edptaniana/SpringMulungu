3d29d9f9c7f7005446715f5f2fc8d856caa245d0
vinniciuspires <99908751+0101vinni@users.noreply.github.com>
2022-11-30 14:20:41 -0300

    Add files via upload
    add metodo put.

--- demo/src/main/java/com/mulungu/demo/controller/UsuarioController.java
+++ demo/src/main/java/com/mulungu/demo/controller/UsuarioController.java
@@ -2,13 +2,20 @@ package com.mulungu.demo.controller;
 
 import com.mulungu.demo.Model.Usuario;
 import com.mulungu.demo.Repository.UsuarioRepository;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+import static javax.swing.UIManager.get;
+
 @RestController
 @CrossOrigin
+@Getter
+@Setter
 @RequestMapping("/usuario")
 public class UsuarioController {
 
@@ -25,6 +32,13 @@ public class UsuarioController {
 
         return usuarioRepository.findAll();
     }
+    @PutMapping("/edit")
+    @ResponseBody
+    public ResponseEntity<String> edit(@RequestParam Long senhaUsuario) {
+        usuarioRepository.save(Usuario.get(senhaUsuario));
+        return new ResponseEntity<String>("Usuario editado com sucesso", HttpStatus.OK);
+    }
+
     @DeleteMapping("/delete")
     @ResponseBody
     public ResponseEntity<String> delete (@RequestParam Long id) {
